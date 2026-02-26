4f893b3c0536bfeaf9690d833e7ebe81b455333a
Nauman <ijaznauman68@gmail.com>
2026-02-26 15:03:30 +0000

    Refactored ConcreteMealFactory to use registry-based creation (removed switch for better OCP compliance)

--- src/main/java/com/naman/restaurant/factory/ConcreteMealFactory.java
+++ src/main/java/com/naman/restaurant/factory/ConcreteMealFactory.java
@@ -3,21 +3,29 @@ package com.naman.restaurant.factory;
 import com.naman.restaurant.model.BaseMeal;
 import com.naman.restaurant.model.Meal;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public class ConcreteMealFactory implements MealFactory {
+
+    private Map<String, Supplier<Meal>> mealRegistry = new HashMap<>();
+
+    public ConcreteMealFactory() {
+
+        mealRegistry.put("pizza", () -> new BaseMeal("Pizza", 10.0));
+        mealRegistry.put("burger", () -> new BaseMeal("Burger", 8.0));
+    }
 
     @Override
     public Meal createMeal(String type) {
 
-        switch (type.toLowerCase()) {
+        Supplier<Meal> mealSupplier = mealRegistry.get(type.toLowerCase());
 
-            case "pizza":
-                return new BaseMeal("Pizza", 10.0);
-
-            case "burger":
-                return new BaseMeal("Burger", 8.0);
-
-            default:
-                throw new IllegalArgumentException("Unknown meal type");
+        if (mealSupplier != null) {
+            return mealSupplier.get();
         }
+
+        throw new IllegalArgumentException("Unknown meal type: " + type);
     }
 }
